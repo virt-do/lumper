abb581d261752755dcf21c9e02497566318e95b8
Samuel Ortiz <s.ortiz@apple.com>
2021-11-26 19:56:43 +0100

    lumper: Add a basic CLI
    Signed-off-by: Samuel Ortiz <s.ortiz@apple.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
+use std::u32;
+
+use clap::Parser;
 use vmm::VMM;
+
+#[derive(Parser)]
+#[clap(version = "0.1", author = "Polytech Montpellier - DevOps")]
+struct VMMOpts {
+    /// Linux kernel path
+    #[clap(short, long)]
+    kernel: String,
+
+    /// Number of virtual CPUs assigned to the guest
+    #[clap(short, long, default_value = "1")]
+    cpus: u8,
+
+    /// Memory amount (in MBytes) assigned to the guest
+    #[clap(short, long, default_value = "512")]
+    memory: u32,
+
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+}
 
 #[derive(Debug)]
 pub enum Error {
@@ -10,6 +33,8 @@ pub enum Error {
 }
 
 fn main() -> Result<(), Error> {
+    let opts: VMMOpts = VMMOpts::parse();
+
     // Create a new VMM
     let mut vmm = VMM::new().map_err(Error::VmmNew)?;
 
@@ -17,7 +42,7 @@ fn main() -> Result<(), Error> {
     // * Number of virtual CPUs
     // * Memory size (in MB)
     // * Path to a Linux kernel
-    vmm.configure(1, 1024, "/path/to/kernel/vmlinux.bin")
+    vmm.configure(opts.cpus, opts.memory, &opts.kernel)
         .map_err(Error::VmmConfigure)?;
 
     // Run the VMM
