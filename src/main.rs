cf2e7093b77ea8150b41796251da2f1a1efd3a58
Samuel Ortiz <s.ortiz@apple.com>
2021-11-26 19:36:06 +0100

    lumper: Initial main implementation
    Build -> Configure -> Run (and fail for now).
    
    Signed-off-by: Samuel Ortiz <s.ortiz@apple.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use vmm::VMM;
+
 #[derive(Debug)]
-pub enum Error {}
+pub enum Error {
+    VmmNew(vmm::Error),
+
+    VmmConfigure(vmm::Error),
+
+    VmmRun(vmm::Error),
+}
 
 fn main() -> Result<(), Error> {
+    // Create a new VMM
+    let mut vmm = VMM::new().map_err(Error::VmmNew)?;
+
+    // Configure the VMM:
+    // * Number of virtual CPUs
+    // * Memory size (in MB)
+    // * Path to a Linux kernel
+    vmm.configure(1, 1024, "/path/to/kernel/vmlinux.bin")
+        .map_err(Error::VmmConfigure)?;
+
+    // Run the VMM
+    vmm.run();
+
     Ok(())
 }
